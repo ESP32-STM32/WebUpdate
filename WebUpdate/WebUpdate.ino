cb2457eb20b28e8e4cfba13d9d9aceaef055b406
ESP32&STM32 <159030581+ESP32-STM32@users.noreply.github.com>
2024-04-12 21:09:29 +0300

    Update WebUpdate.ino

--- WebUpdate/WebUpdate.ino
+++ WebUpdate/WebUpdate.ino
@@ -7,3 +7,24 @@ void setup(void) {
 void loop(void) {
   handleUP();
 }
+
+/* #define LED 2
+
+unsigned long tmrLED;
+int ledState = 0;
+
+void setup(void) {
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+}
+
+void loop(void) {
+  led();
+}
+
+void led() {
+  if (millis() - tmrLED >= 500) {
+    tmrLED = millis();
+    digitalWrite(LED, ledState = !ledState);
+  }
+} */
